faa1fd449dafa6a33fc168f8c06ea68a5e32b1c1
dsmiff <dominic.smith@bristol.ac.uk>
2015-04-21 21:09:28 +0200

    main working code

---
+++ home.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="/dosmith/page/external/styles/screen.css"  type="text/css" media="screen, projection" />
+<link rel="stylesheet" href="/dosmith/page/external/styles/print.css"  type="text/css" media="print" />
+<link rel="stylesheet" href="/dosmith/page/external/css/elusive-icons.min.css" />
+<link rel="stylesheet" href="/dosmith/page/external/site_style/screen.css"  type="text/css" media="screen, projection" />  
+<link rel="stylesheet" href="/dosmith/page/external/site_style/layout.css"  type="text/css" media="screen, projection" />
+<!-- typekit -->
+<!-- <script type="text/javascript" src="//use.typekit.net/jfj5gcv.js"></script> -->
+   <!-- <script type="text/javascript">try{Typekit.load();}catch(e){}</script> -->
+<!-- end typekit -->
+<title>DomFolio</title>
+<link rel="shortcut icon" href="/dosmith/page/images/favicon.ico">
+<link rel="icon" type="image/gif" href="animated_favicon1.gif">
+<link href="/dosmith/page/site_packages/prettify.css"  type="text/css" rel="stylesheet" />
+<link href="/dosmith/page/site_packages/prettify_custom.css"  type="text/css" rel="stylesheet" />
+<script type="text/javascript" src="/dosmith/page/external/jquery/prettify.js" ></script>
+<script>
+							     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+								 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+															    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+																					       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-55015071-2', 'auto');
+ga('send', 'pageview');
+
+</script>
+</head>
+<body onload="prettyPrint()">
+<div class="container">
+  <div class="span-24 last">
+    <div id="header">
+      <div class="span-22">
+<h1><a href="https://github.com/dsmiff" >DomsFolio</a> </h1>
+<div class="description">Dom Smith</div>
+</div>
+<div class="span-2 append-bottom last">
+<i class="el el-bookmark el-1x" style="color:#0b4465; vertical-align:top;"></i>
+<i class="el el-book el-2x" style="color:#0b4465; vertical-align:top;"></i>
+</div>
+    </div><!--header--> 
+  </div>
+</div><!--container -->
+
+<div id="subheader">
+<div class="container">
+<div class="span-24 last">
+<div id="subheadernavi">
+  <ul>
+  <a href="https://github.com/dsmiff/cmg-cmssw-private"><i class="el el-lock" style="font-size:0.6em"></i>&nbsp;SUSY &alpha;<sub>T</sub>
+  <span> // </span>
+  <dd> <a href="/dosmith/page/plots"> <i class="el el-lock" style="font-size:0.5em"></i>&nbsp; Plots</a> </dd>
+  <dd> <a href="https://twiki.cern.ch/twiki/bin/viewauth/CMS/AlphaT"> <i class="el el-lock" style="font-size:0.5em"></i>&nbsp; TWiki</a> </dd>
+  <dd> <a href="https://github.com/CMSRA1/"> <i class="el el-lock" style="font-size:0.5em"></i>&nbsp; GitHub</a> </dd>
+  </a>
+  <a href="http://l1ts-gct.cms:2974/urn:xdaq-application:service=hyperdaq"><i class="el el-lock" style="font-size:0.6em"></i>&nbsp;Trigger Commissioning & GCT</a>
+  <span> // </span>
+  <dd> <a href="https://twiki.cern.ch/twiki/bin/view/CMS/OnlineWBTriggerContacts"> <i class="el el-lock" style="font-size:0.5em"></i>&nbsp; Contacts</a> </dd>
+  <a href="https://github.com/dsmiff/cmssw-old/tree/tpinspect"><i class="el el-lock" style="font-size:0.6em"></i>&nbsp;HCal DPG Trigger</a>
+  <span> // </span>
+  <dd> <a href="https://twiki.cern.ch/twiki/bin/viewauth/CMS/HcalDPGTrigger"> <i class="el el-lock" style="font-size:0.5em"></i>&nbsp; TWiki</a> </dd>
+  <a href="https://github.com/InvisibleHiggsUK"><i class="el el-lock" style="font-size:0.6em"></i>&nbsp;Invisible Higgs</a>
+  <span> // </span>
+  <br />
+  <a href="https://github.com/dsmiff/Old-useful-codes/tree/master/ILC"> <i class="el el-lock" style="font-size:0.6em"></i>&nbsp;ILC</a>
+  <span> // </span>
+  <br />
+  <a href="https://cmsweb.cern.ch/das/"><i class="el el-lock"  style="font-size:0.6em"></i>&nbsp;Datasets</a>
+  <span> // </span>
+  <br />
+  <a href="https://github.com/dsmiff/cmssw-old"><i class="el el-lock" style="font-size:0.6em"></i>&nbsp;CMSSW</a>
+  </ul>
+<div class="clear"></div>
+</div><!--subheadernavi--> 
+</div>
+</div><!--container -->
+</div><!--subheader--> 
+<div class="container">
+<div class="span-24 torse">
+
+<div class="span-24 last">
+<div id="content">
+
+<img style="float: right; padding: 0 1px" src="/dosmith/page/images/GitHubAvatar.png" alt="" width="200" />
+
+<b>Contact</b><br />
+Dominic Smith<br />
+dosmith@cern.ch<br />
+(+44)75 95269377<directlink id="dl_8744707" link="http://https://dosmith.web.cern.ch/dosmith/page/home.php/"></directlink></div></div>
+
+<br />
+
+<div class="clear">
+</div>
+</div><!--content--> 
+</div>
+</div><!--torso--> 
+<div class="span-24 last"> 
+<div id="footer"> 											    &copy;2015 Dominic Smith
+</div><!--footer--> 
+</div>
+</div><!--container -->
+</body>
+</html>
